5c36580a62149fd0a4fc98b99aa83129ce689935
NickaelCalldas <nickaelcalldas@gmail.com>
2022-05-05 17:22:42 -0300

    Criamos a classe modelo da aplicação

--- src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -8,7 +8,6 @@ public class BanklineApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
 	}
-	//teste a aplicação com github
-	// teste 2
+	
 
 }
