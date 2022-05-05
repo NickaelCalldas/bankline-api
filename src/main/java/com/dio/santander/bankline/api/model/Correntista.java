aea03e166777d1b86bf190268a53282237cabb49
NickaelCalldas <nickaelcalldas@gmail.com>
2022-05-05 17:45:17 -0300

    Mapenado as Entidades com JPA

--- src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -1,6 +1,16 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tab_correntista")
 public class Correntista {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String cpf;
 	private String nome;
--- src/main/java/com/dio/santander/bankline/api/model/TipoMovimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/TipoMovimentacao.java
@@ -1,5 +1,7 @@
 package com.dio.santander.bankline.api.model;
 
-public class TipoMovimentacao {
+public enum TipoMovimentacao {
+	RECEITA,
+	DESPESA
 
 }
