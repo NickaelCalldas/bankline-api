9b1eaae2142c3fc309aa4c3efeed4c0a998e0973
NickaelCalldas <nickaelcalldas@gmail.com>
2022-05-17 20:11:13 -0300

    atualização

--- src/main/java/com/dio/santander/bankline/api/repository/Movimentacao_repository.java
+++ src/main/java/com/dio/santander/bankline/api/repository/Movimentacao_repository.java
@@ -1,9 +1,11 @@
 package com.dio.santander.bankline.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dio.santander.bankline.api.model.Movimentacao;
 
 public interface Movimentacao_repository extends JpaRepository<Movimentacao, Integer>{
-
+	public List<Movimentacao>findByIdConta(Integer idConta);
 }
